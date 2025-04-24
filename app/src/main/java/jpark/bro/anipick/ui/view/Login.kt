3bc60a764bc293b88548a94cfd496cb5a0e69f52
jparkbro <jparkbro@gmail.com>
2025-04-24 21:33:30 +0900

    login component Full Img > Symbol

--- app/src/main/java/jpark/bro/anipick/ui/view/Login.kt
+++ app/src/main/java/jpark/bro/anipick/ui/view/Login.kt
@@ -1,6 +1,7 @@
 package jpark.bro.anipick.ui.view
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -18,6 +20,7 @@ import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -50,7 +53,7 @@ fun Login() {
                 painter = painterResource(R.drawable.anipick_logo),
                 contentDescription = "",
                 modifier = Modifier
-                    .size(width = 120.dp, height = 24.dp)
+                    .size(width = 120.dp, height = 32.dp)
             )
             Spacer(modifier = Modifier.height(48.dp))
             Text(
@@ -70,22 +73,59 @@ fun Login() {
         /**
          * Mid Login Section
          */
-        Column {
-            Image(
-                painter = painterResource(R.drawable.kakao_login),
-                contentDescription = "",
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Row(
                 modifier = Modifier
-                    .fillMaxWidth()
+                    .width(360.dp)
                     .height(64.dp)
-            )
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(Color(0xFFFEE500)),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.kakao_symbol),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(28.dp)
+                )
+                Spacer(modifier = Modifier.width(24.dp))
+                Text(
+                    text = "카카오 계정으로 계속하기",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.W600,
+                    color = Color(0xFF3A1E20)
+                )
+            }
             Spacer(modifier = Modifier.height(12.dp))
-            Image(
-                painter = painterResource(R.drawable.google_login),
-                contentDescription = "",
+            Row(
                 modifier = Modifier
-                    .fillMaxWidth()
+                    .width(360.dp)
                     .height(64.dp)
-            )
+                    .clip(RoundedCornerShape(8.dp))
+                    .border(1.dp, Color(0xFFE3E3E3), RoundedCornerShape(8.dp))
+                    .background(Color(0xFFFFFFFF)),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.google_symbol),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(28.dp)
+                )
+                Spacer(modifier = Modifier.width(24.dp))
+                Text(
+                    text = "구글 계정으로 계속하기",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.W600,
+                    color = Color(0xFF3A1E20)
+                )
+            }
             Spacer(modifier = Modifier.height(24.dp))
             Row(
                 modifier = Modifier
