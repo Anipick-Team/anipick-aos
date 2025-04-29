643501acb1dccbfe924f3e77f3e44ebdcb0aa808
jparkbro <jparkbro@gmail.com>
2025-04-29 18:44:42 +0900

    구글로그인 인증관리자 하단시트 > 구글 계정으로 로그인 으로 ui 수정

--- app/src/main/java/jpark/bro/anipick/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/jpark/bro/anipick/data/repository/AuthRepositoryImpl.kt
@@ -6,12 +6,14 @@ import android.util.Log
 import androidx.credentials.CredentialManager
 import androidx.credentials.GetCredentialRequest
 import androidx.credentials.exceptions.GetCredentialException
-import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GetSignInWithGoogleOption
 import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
 import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
 import dagger.hilt.android.qualifiers.ApplicationContext
-import jpark.bro.anipick.data.model.KakaoSignInRequest
 import jpark.bro.anipick.data.model.UserData
 import jpark.bro.anipick.domain.model.User
 import jpark.bro.anipick.domain.repository.AuthRepository
@@ -19,8 +21,6 @@ import jpark.bro.anipick.domain.util.Result
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.map
-import java.io.IOException
-import java.lang.ref.WeakReference
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -51,11 +51,8 @@ class AuthRepositoryImpl @Inject constructor(
     private val credentialManager by lazy { CredentialManager.create(context) }
 
     // Google ID 옵션 생성
-    private fun createGoogleIdOption(): GetGoogleIdOption {
-        return GetGoogleIdOption.Builder()
-            .setFilterByAuthorizedAccounts(false) // true면 이미 로그인한 계정만 표시
-            .setServerClientId(WEB_CLIENT_ID)
-            .setAutoSelectEnabled(true) // 계정이 하나만 있을 경우 자동 선택
+    private fun createGoogleIdOption(): GetSignInWithGoogleOption {
+        return GetSignInWithGoogleOption.Builder(WEB_CLIENT_ID)
             .build()
     }
 
@@ -105,15 +102,38 @@ class AuthRepositoryImpl @Inject constructor(
     override suspend fun signInWithKakao(activity: Activity) {
         _authState.value = Result.Loading
 
-        // 카카오계정으로 로그인
-        UserApiClient.instance.loginWithKakaoTalk(activity) { token, error ->
+        // 카카오계정으로 로그인 공통 callback 구성
+        // 카카오톡으로 로그인 할 수 없어 카카오계정으로 로그인할 경우 사용됨
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
             if (error != null) {
-                Log.e("kakao login", "로그인 실패", error)
-            }
-            else if (token != null) {
-                Log.i("kakao login", "로그인 성공 ${token.accessToken}")
+                Log.e("kakao login", "카카오계정으로 로그인 실패", error)
+            } else if (token != null) {
+                Log.i("kakao login", "카카오계정으로 로그인 성공 ${token.accessToken}")
             }
         }
+
+        // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(activity)) {
+            UserApiClient.instance.loginWithKakaoTalk(activity) { token, error ->
+                if (error != null) {
+                    Log.e("kakao login", "카카오톡으로 로그인 실패", error)
+
+                    // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                    // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                        return@loginWithKakaoTalk
+                    }
+
+                    // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                    UserApiClient.instance.loginWithKakaoAccount(activity, callback = callback)
+                } else if (token != null) {
+                    Log.i("kakao login", "카카오톡으로 로그인 성공 ${token.accessToken}")
+                }
+            }
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(activity, callback = callback)
+        }
+
     }
 
 
